7ff0130dc18c1b36e93ca6d7c13845fe6fe46d35
JaimeCuenca <jaime.cuenca.caro@gmail.com>
2020-11-27 11:11:50 +0100

    final

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,50 +5,45 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-import java.awt.event.ActionEvent;
 import java.io.IOException;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Controller {
-    @FXML
-    private Label label;
-    @FXML
-    private TextField eT;
-    @FXML
-    private Button boton;
-
-    int vez=0;
 
     @FXML
-    public void setTexto() {
-        label.setText(String.valueOf(ThreadLocalRandom.current().nextInt(0, 100)));
+    private Button b1;
+    @FXML
+    private Button b2;
+    @FXML
+    private Button b3;
+    @FXML
+    private Button b4;
+    @FXML
+    private Button b5;
+
+    int clicks=0;
+    List<String> numeros = Arrays.asList("1", "2", "3", "4", "5");
+
+    @FXML
+    public void numerosAleatorios() {
+        Collections.shuffle(numeros);
+        b1.setText(numeros.get(0));
+        b2.setText(numeros.get(1));
+        b3.setText(numeros.get(2));
+        b4.setText(numeros.get(3));
+        b5.setText(numeros.get(4));
     }
 
     @FXML
-    public void onTextoEditado(){
-        vez++;
-        if(vez%2!=0) {
-            if (label.getText().equals(eT.getText())) {
-                boton.setVisible(true);
-                setTexto();
-            } else {
-                boton.setVisible(false);
-            }
-        }else{
-            if(label.getText().equals(eT.getText())){
-                boton.setDisable(false);
-            }else{
-                boton.setDisable(true);
-            }
-        }
-    }
-    @FXML
-    public void onBotonClickado(){
-        try{
+    public void onBotonClickado(Button b){
+
+
+
+            try{
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("sample2.fxml"));
             Parent root = fxmlLoader.load();
             Stage stage = new Stage();
